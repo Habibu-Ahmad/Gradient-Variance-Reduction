df3be8e421ec66631043e8dd32dfb23d2f6e2e98
Ahmad Habibu <hadandume@gmail.com>
2025-07-18 18:02:29 +0200

    Create run-sgd-cifar100.sh

---
+++ scripts/run-sgd-cifar100.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Train SGD on CIFAR-100 with ResNet-18
+
+python train.py \
+    --arch resnet18 \
+    --datasets CIFAR100 \
+    --epochs 2 \
+    --batch-size 128 \
+    --lr 0.05 \
+    --momentum 0.9 \
+    --weight-decay 5e-4 \
+    --cutout \
+    --alpha 0.01 \
+    --optimizer sgd \
+    --seed 42 \
+    --gpu 0 \
+    --log-file sgd_resnet18_cifar100.log \
+    --data-root ./datasets/
